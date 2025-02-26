d2aac393ec4925b780af77da24b8413723a7ced0
Alex Rumak <alexanderrumak@gmail.com>
2025-02-26 09:12:19 -0500

    Initial commit

---
+++ CraftingInterpreters/Chapter1/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+///////// LINKED LIST ////////////
+// Define a doubly linked list of heap allocated strings
+
+struct Node 
+{
+  char *val;
+  struct Node *next;
+  struct Node *prev;
+};
+
+
+
+
+int main(void)
+{
+
+
+  return 0;
+}
